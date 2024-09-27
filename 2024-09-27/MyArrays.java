bc29269ee68ab1435781252fe7a919f49d9c52b9
Jessie Wang <jessie.w2580@gmail.com>
2024-09-27 10:15:18 -0400

    returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,21 +1,28 @@
 public class MyArrays {
   public static String aryToString(int[] nums) {
     String result = "[";
-    for (int i=0; i<nums.length-1;i++) {
-      result += nums[i]+", ";
+    for (int i=0; i<nums.length;i++) {
+      if (i == nums.length) {
+        result += nums[i];
+      }
+      result += nums[i] + ", ";
     }
     return result+"]";
   }
+
+
   public static int[] returnCopy(int[] ary) {
-
-  }
-
-  public static int[] concatArray(int[] ary1,int[] ary2) {
-
+    int[] copy = new int(ary.length);
+    for (int i=0; i<ary.length; i++) {
+      copy[i] = ary[i];
+    }
+    return copy;
   }
 
 
   public static void main (String[] args) {
-
+    int[] TestArray = new int {1,2,3,4};
+    System.out.print(aryToString(TestArray));
+    System.out.println(returnCopy(TestArray));
   }
 }
