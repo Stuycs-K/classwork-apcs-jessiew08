0198f8cd9a372f0538a9f659095c963c790b4632
Jessie Wang <jessie.w2580@gmail.com>
2024-09-27 10:04:43 -0400

    aryToString

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,4 +1,11 @@
 public class MyArrays {
+  public static String aryToString(int[] nums) {
+    String result = "[";
+    for (int i=0; i<nums.length-1;i++) {
+      result += nums[i]+", ";
+    }
+    return result+"]";
+  }
   public static int[] returnCopy(int[] ary) {
 
   }
