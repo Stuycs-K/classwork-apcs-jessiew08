eea53b7d111b7b68334fc6ea51d71cfbb11171f5
Jessie Wang <jessie.w2580@gmail.com>
2024-09-27 20:07:28 -0400

    MoreTests

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -39,9 +39,20 @@ public class MyArrays {
     System.out.println("Copy: " + aryToString(copy));
     System.out.println("Check: " + (original == copy));
 
+    int[] original2 = new int[] {13,15,6,7,0};
+    int[] copy2 = returnCopy(original);
+    System.out.println("Original: " + aryToString(original2));
+    System.out.println("Copy: " + aryToString(copy2));
+    System.out.println("Check: " + (original2 == copy2));
+
     int[] ary1 = new int[] {1,2,3,4};
     int[] ary2 = new int[] {5,6,7,8};
     int[] concat = concatArray(ary1, ary2);
     System.out.println("Concatenated: " + aryToString(concat)); 
+
+    int[] ary3 = new int[] {15,61,7,10};
+    int[] ary4 = new int[] {101,12,13};
+    int[] concat2 = concatArray(ary3, ary4);
+    System.out.println("Concatenated: " + aryToString(concat2)); 
   }
 }
