9c1106fda7a07ee096cef3f3085e2029bd285168
Jessie Wang <jessie.w2580@gmail.com>
2024-10-15 09:43:55 -0400

    Point contructor

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -8,6 +8,8 @@ public class Point{
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
     //COMPLETE THIS SECOND
+    x = other.x;
+    y = other.y;
     //Then write test cases.
   }
 
