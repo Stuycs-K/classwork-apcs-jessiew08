940f477c9596a36885c7f9fd5660dd30fa6eec89
Jessie Wang <jessie.w2580@gmail.com>
2024-11-04 10:04:22 -0500

    wrote countTrianglesB

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -28,10 +28,33 @@ public class TriangleTester{
     return count;
   }
 
+  public static int countTrianglesB(String filename) {
+    int count = 0;
+    try (Scanner scanner = new Scanner(new File(filename))) {
+      while (scanner.hasNextLine()) {
+        int [][] triangles = new int[3][3];
+        for (int i=0; i<2; i++){
+          Scanner triangle = new Scanner(scanner.nextLine());
+          for (int j=0; j<2; j++){
+            triangles[i][j] = triangle.nextInt();
+          }
+        }
+        for (int i=0; i<2; i++){
+          if (isValidTriangle(triangles[0][i], triangles[1][i], triangles[2][i])){
+            count++;
+          }
+        }
+      }
+      return count;
+    }
+    catch (FileNotFoundException e) {
+    return 0;}
+  }
+
 
 
   public static void main(String[] args) {
     System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
   }
 }
-    
