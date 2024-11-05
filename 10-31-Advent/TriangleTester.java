8c931c0e0c72047fa9067746e0ac075d60bdf7d7
Jessie Wang <jessie.w2580@gmail.com>
2024-11-04 20:11:43 -0500

    countTrianglesA with parseInt

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -51,10 +51,29 @@ public class TriangleTester{
     return 0;}
   }
 
+  public static int countTrianglesAWithParseInt(String filename) {
+    int count = 0;
+    try (Scanner scanner = new Scanner(new File(filename))) {
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        String[] nums = line.split(" +"); 
+        int a = Integer.parseInt(nums[0]);        
+        int b = Integer.parseInt(nums[1]);
+        int c = Integer.parseInt(nums[2]);
 
+        if (isValidTriangle(a, b, c)) {
+          count++;
+        }
+      }
+    } catch (FileNotFoundException e) {
+      return 0;
+    }
+    return count;
+  }
 
   public static void main(String[] args) {
     System.out.println(countTrianglesA("inputTri.txt"));
     System.out.println(countTrianglesB("inputTri.txt"));
+    System.out.println(countTrianglesAWithParseInt("inputTri.txt"));
   }
 }
