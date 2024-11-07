5d932b86c3ab57d9909e7c5f7bf015097a8d5346
Jessie Wang <jessie.w2580@gmail.com>
2024-11-07 18:53:00 -0500

    answersToStep10

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,5 +1,9 @@
+//Step 8:
 //When super.speak is called from Bird in speak, the getName in Animal is overridden by the new getName in Bird.
 //Therefore the name that is printed has the changes that were made to getName in Bird.java.
+//Step 10:
+// Bird b2 = new Animal() didn't work because Animal is not a subclass of Bird, meaning that Animal-is-a-Bird is false.
+// But, Animal a1 = new Animal(); and Animal a2 = new Bird(); work because of that Bird-is-a-Animal, meaning that Bird is a subclass of Animal
 public class Driver{
   public static void main(String[] args){
     Animal a1 = new Animal("Moo", 5, "Cow");
@@ -7,5 +11,11 @@ public class Driver{
 
     Bird b1 = new Bird("Chirp", 2, "Bird", 5, "Blue");
     b1.speak();
+
+    //Bird b2 = new Animal("Woof", 2, "Dog");
+    //b2.speak();
+
+    Animal a2 = new Bird("Tweet", 3, "Sam", 4, "Red");
+    a2.speak();
   }
 }
