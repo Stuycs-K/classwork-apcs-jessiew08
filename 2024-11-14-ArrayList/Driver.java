05781156b61cee8155f76804ca92034d63f955bd
Jessie Wang <jessie.w2580@gmail.com>
2024-11-14 18:24:34 -0500

    partnerWorkDriver

---
+++ 2024-11-14-ArrayList/Driver.java
@@ -0,0 +1,20 @@
+//Amy Zhou
+//Jessie Wang
+import java.util.ArrayList;
+public class Driver {
+  public static void main (String[] args) {
+    ArrayList<String>a1;
+    a1 = ArrayListPractice.createRandomArray(50);
+    System.out.println(a1);
+    ArrayListPractice.replaceEmpty(a1);
+    System.out.println(a1);
+    ArrayList<String>a2;
+    a2 = ArrayListPractice.makeReversedList(a1);
+    System.out.println(a2);
+    ArrayList<String> a3;
+    a3 = ArrayListPractice.createRandomArray(22);
+    ArrayList<String> a4;
+    a4 = ArrayListPractice.mixLists(a2,a3);
+    System.out.println(a4);
+  }
+}
