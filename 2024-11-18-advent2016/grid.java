3fb5873f51283438b8c1e9043229432c261de466
Jessie Wang <jessie.w2580@gmail.com>
2024-11-18 10:13:35 -0500

    day1_codeStarted

---
+++ 2024-11-18-advent2016/grid.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class grid{
+  public static int countBlocks(String filename){
+    int count = 0;
+    try (Scanner scanner = new Scanner(new File(filename)){
+
+
+    }
+  }
+}
