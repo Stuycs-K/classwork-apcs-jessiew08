64a2950b92e9b3190056956e6fa2d6dd4769b4ab
Jessie <jessie.w2580@gmail.com>
2024-11-19 18:18:28 -0500

    Update and rename grid.java to day1.java

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class grid {
+public class day1{
   public static int countBlocks(String filename) {
     int count = 0;
     try (Scanner scanner = new Scanner(new File(filename))) {
