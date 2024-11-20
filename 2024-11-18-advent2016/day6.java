6038da70c1c2a2c8bb91271530433d6c89684d04
Jessie Wang <jessie.w2580@gmail.com>
2024-11-20 10:14:46 -0500

    startedDay6

---
+++ 2024-11-18-advent2016/day6.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class day6{
+  public static String corruptMessage(String filename){
+    String message = "";
+    try (Scanner scanner = new Scanner(new File(filename))){
+      while(scanner.hasNext()){
+        
+      }
+
+  }
+}
