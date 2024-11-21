25ba54ef5375e0d6d3d267b1d06f9480e0486ac3
Jessie Wang <jessie.w2580@gmail.com>
2024-11-20 22:10:52 -0500

    finishedPart2

--- 2024-11-18-advent2016/day2.java
+++ 2024-11-18-advent2016/day2.java
@@ -37,11 +37,46 @@ public class day2{
 
   public static void part2(String filename) {
     char[][] keypad = {{' ', ' ', '1', ' ', ' '},{' ', '2', '3', '4', ' '},{'5', '6', '7', '8', '9'},{' ', 'A', 'B', 'C', ' '},{' ', ' ', 'D', ' ', ' '}};
+    int x = 0; 
+    int y = 2;
+    
+    try (Scanner fileScanner = new Scanner(new File(filename))) {
+      while (fileScanner.hasNextLine()) {
+        String instructions = fileScanner.nextLine();
+        for (int i = 0; i < instructions.length(); i++) {
+          char move = instructions.charAt(i);
+
+          if (move == 'U') {
+            if (x > 0 && keypad[x - 1][y] != ' ') {
+              x--;
+            }
+          } else if (move == 'D') {
+            if (x < 4 && keypad[x + 1][y] != ' ') {
+              x++;
+            }
+          } else if (move == 'L') {
+            if (y > 0 && keypad[x][y - 1] != ' '){
+              y--;
+            } 
+          } else if (move == 'R') {
+            if (y < 4 && keypad[x][y + 1] != ' ') {
+              y++;
+            } 
+          }
+        }
+
+        System.out.print(keypad[x][y]);
+      }
+
+      System.out.println(); 
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found: " + filename);
+    }
+  }
 
 
   public static void main(String[] args) {
     String filename = "instructions.txt"; 
-
-    bathroomCode(filename);
+    part2(filename);
   }
 }
