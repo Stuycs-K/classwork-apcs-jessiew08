2ac0855d3c7dab38bc567c1f71d0244309d0e6fd
Jessie Wang <jessie.w2580@gmail.com>
2024-11-20 19:20:31 -0500

    workedOnDay6

--- 2024-11-18-advent2016/day6.java
+++ 2024-11-18-advent2016/day6.java
@@ -6,9 +6,38 @@ public class day6{
   public static String corruptMessage(String filename){
     String message = "";
     try (Scanner scanner = new Scanner(new File(filename))){
-      while(scanner.hasNext()){
-        
+      String[] lines = new String[0];
+      int lineCount = 0;
+
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        if (lines.length == 0) {
+          lines = new String[1];
+        } else if (lineCount == lines.length) {
+          String[] newLines = new String[lines.length * 2];
+          for (int i = 0; i < lines.length; i++) {
+            newLines[i] = lines[i];
+          }
+          lines = newLines;
+        }
+        lines[lineCount++] = line;
       }
 
+      String[] validLines = new String[lineCount];
+      for (int i = 0; i < lineCount; i++) {
+        validLines[i] = lines[i];
+      }
+      lines = validLines;
+
+      int columns = lines[0].length();
+      int[][] freq = new int[columns][26];
+    }
+  }
+
+  public static void main(String[] args) {
+    String filename = "message.txt";
+    System.out.println(corruptMessage(filename));
   }
 }
+
+
