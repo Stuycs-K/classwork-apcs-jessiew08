d696f1b2a660709ffb280cbf36199f41f0cdc84a
Jessie Wang <jessie.w2580@gmail.com>
2024-12-03 20:20:08 -0500

    homework_madeMoreIntere sting

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -16,10 +16,11 @@ public class ColorDemo{
   public static void main(String[] args){
     for(int r = 0; r < 256; r+=32){
     for(int g = 0; g <= 256; g+=32){
-    for(int b = 0; b <= 256; b+=32){
-    System.out.print("\u001b[38;2;"+r+";"+b+";"+g+";7m.");
+    for(int b = 0; b <= 256; b+=37){
+    System.out.print("\u001b[38;2;"+g+";"+b+";"+r+";7m:o");
       }
     }
+    System.out.print("\u001b[0m");
     System.out.println();
     }
   }
