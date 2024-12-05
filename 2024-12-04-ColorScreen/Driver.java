d2c4224f2373e6cb5a7d53f1a600a810a0508528
Jessie Wang <jessie.w2580@gmail.com>
2024-12-04 21:52:14 -0500

    displayRandomNumbers

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -13,3 +13,20 @@ public class Driver{
       System.out.println();
     }
   }
+
+  public static void displayRandomNumbers(int[] numbers, int width) {
+    System.out.print("\033[1;2H");
+    int space = width / (numbers.length + 1);
+    for (int i = 0; i < numbers.length; i++) {
+      int x = (i + 1) * space - 1;
+      System.out.print("\033[" + x + "G");
+      if (numbers[i] < 25) {
+        System.out.print("\u001B[91m" + numbers[i] + "\u001B[0m");
+      } else if (numbers[i] > 75) {
+        System.out.print("\u001B[92m" + numbers[i] + "\u001B[0m");
+      } else {
+        System.out.print("\u001B[37m" + numbers[i] + "\u001B[0m");
+      }
+    }
+    System.out.println();
+  }
