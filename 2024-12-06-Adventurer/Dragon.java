251c452000c6ee7033d2688041d2c2e93d3cf85c
Jessie Wang <jessie.w2580@gmail.com>
2024-12-07 15:30:10 -0500

    attacksAndsupportMethods

--- 2024-12-06-Adventurer/Dragon.java
+++ 2024-12-06-Adventurer/Dragon.java
@@ -1,4 +1,4 @@
-public class Dragon extends Adventurer {
+public abstract class Dragon extends Adventurer {
   private int rage;
 
   public Dragon(String name) {
@@ -27,3 +27,42 @@ public class Dragon extends Adventurer {
   public int getSpecialMax() {
     return 100; 
   }
+
+  public String attack(Adventurer other) {
+    if (getSpecial() >= 5) {
+      setSpecial(getSpecial() - 5);
+      other.applyDamage(15);
+      return getName() + " swipes at " + other.getName() + ", dealing 15 damage and spending 5 rage!";
+    } else {
+      return getName() + " doesn't have enough rage to attack!";
+    }
+  }
+
+  public String specialAttack(Adventurer other) {
+    if (getSpecial() >= 20) {
+      setSpecial(getSpecial() - 20);
+      other.applyDamage(40);
+      return getName() + " unleashes a fiery breath on " + other.getName() + ", dealing 40 damage and spending 20 rage!";
+    } else {
+      return getName() + " doesn't have enough rage to perform a special attack!";
+    }
+  }
+
+  public String support() {
+    if (getSpecial() <= getSpecialMax() - 10) {
+      setSpecial(getSpecial() + 10);
+      return getName() + " roars and regains 10 rage!";
+    } else {
+      return getName() + " is already at maximum rage!";
+    }
+  }
+
+  public String support(Adventurer other) {
+    if (getSpecial() <= getSpecialMax() - 10) {
+      setSpecial(getSpecial() + 10);
+      return getName() + " roars and boosts special for allies by 10!";
+    } else {
+      return getName() + " can't boost allies' special any further!";
+    }
+  }
+}
