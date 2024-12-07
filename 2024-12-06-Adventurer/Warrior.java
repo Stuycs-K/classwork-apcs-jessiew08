a75e744f79c32713560332a51675108560fff277
Jessie Wang <jessie.w2580@gmail.com>
2024-12-07 14:11:41 -0500

    fixedMethods

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,29 +1,36 @@
 public abstract class Warrior extends Adventurer{
   public Warrior(String name){
     super(name);
+    this.special = 25;
   }
   public Warrior(String name, int hp){
     super(name, hp);
+    this.special = 25;
   }
   public String getSpecialName(){
-    return ("Strength");
+    return "Strength";
   }
   public int getSpecial() {
     return special;
   }
   public void setSpecial(int num) {
-    special = num;
+    this.special = Math.max(0, Math.min(num, getSpecialMax()));
   }
   public int getSpecialMax() {
     return 50;
   }
   public String attack(Adventurer other){
     other.applyDamage(getSpecial());
-    return "You have damaged " + other.getName() + "by " + getSpecial() + "hp!";
+    return getName() + " attacks " + other.getName() + ", dealing 10 damage!";
   }
   public String specialAttack(Adventurere other){
-    other.applyDamage(20);
-    other.setSpecial(other.getSpecial() - 10);
-    return "You have done a special attack on " + other.getName() + "and damaged them by 20 hp and 10 special!";
-  }
+    if (getSpecial() >= 10){
+      other.applyDamage(20);
+      setSpecial(getSpecial() - 10);
+      return getName() + " performs a special attack on " + other.getName() + ", dealing 20 damage and using 10 Strength!";
+    }
+    else{
+      return getName() + " tries a special attack but lacks Strength!";
+    }
+ }
 }
